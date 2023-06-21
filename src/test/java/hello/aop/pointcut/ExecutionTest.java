9f945faf8ea0057ad28dc898260ce8a55dd829a8
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-06-22 08:25:07 +0900

    [FEAT] 포인트컷 예제 셋팅

---
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -0,0 +1,32 @@
+package hello.aop.pointcut;
+
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import hello.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExecutionTest {
+
+	AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+	Method helloMethod;
+
+	@BeforeEach
+	public void init() throws NoSuchMethodException {
+		helloMethod = MemberService.class.getMethod("hello", String.class);
+	}
+
+	@Test
+	void printMethod() {
+		// public java.lang.String hello.aop.member.MemberService.hello(java.lang.String)
+		log.info("helloMethod={}", helloMethod);
+	}
+
+
+
+}
